f1a8a25742a6553576834c23321f26873a6bed1e
Cheng Gang <chenggangpro@gmail.com>
2019-01-30 11:33:06 +0800

    Change Version

---
+++ src/test/java/pro/chenggang/plugin/springcloud/gateway/test/SpringCloudGatewayPluginApplicationTests.java
@@ -0,0 +1,18 @@
+package pro.chenggang.plugin.springcloud.gateway.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import pro.chenggang.plugin.springcloud.gateway.SpringCloudGatewayPluginApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringCloudGatewayPluginApplication.class)
+public class SpringCloudGatewayPluginApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
